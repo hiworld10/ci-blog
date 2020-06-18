5f25ad6501b9c0940f4d7b6704865a45f3ad7102
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-18 20:59:29 -0300

    Add navbar to header view

--- app/Views/inc/header.php
+++ app/Views/inc/header.php
@@ -1,10 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>CI-Blog</title>
-    <link rel="icon" href="<?=base_url()?>/icon/favicon.ico" type="image/gif">
-    <link rel="stylesheet" href="<?=base_url()?>/css/bootstrap.min.css">
+	<meta charset="UTF-8">
+	<title>CI-Blog</title>
+	<link rel="icon" href="<?=base_url()?>/icon/favicon.ico" type="image/gif">
+	<link rel="stylesheet" href="<?=base_url()?>/css/bootstrap.min.css">
 </head>
 <body>
-    <h1><?= esc($title); ?></h1>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+					<a class="nav-item nav-link" href="#">Features</a>
+				</div>
+			</div>
+		</div>
+	</nav>

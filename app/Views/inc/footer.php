45bf73c702cde09584c8df1a543abf0d5695ee0a
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-30 23:05:14 -0300

    Modify footer file

--- app/Views/inc/footer.php
+++ app/Views/inc/footer.php
@@ -1,4 +1,9 @@
-    <em>&copy; 2020 Trademark Goes Here Ltd. All rights served.</em>
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+<footer class="footer bg-light fixed-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                <p class="text-muted small mb-4 mb-lg-0">&copy; 2020 Trademark Goes Here Ltd. All rights served.</p>
+            </div>
+        </div>
+    </div>
+</footer>
